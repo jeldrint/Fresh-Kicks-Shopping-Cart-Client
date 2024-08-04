fe6f6dbebf72381c6a0d75693a7b1b1ef22deb08
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-08-05 02:59:35 +0800

    still working on sizegrid.
    * this one took too much of my time
    * but at least I have the layout rendered already

--- src/components/SizeGrid.tsx
+++ src/components/SizeGrid.tsx
@@ -2,42 +2,94 @@ import { Shoe_Category } from "../types/shoetype"
 
 type SizeGridProps = {
     category: Shoe_Category,
-    sizeSwitch: string,
-    setSizeSwitch: React.Dispatch<React.SetStateAction<string>>
+    sizeSwitch: string | null,
+    setSizeSwitch: React.Dispatch<React.SetStateAction<string | null>>
 }
 
 const SizeGrid = ({category, sizeSwitch, setSizeSwitch}: SizeGridProps) : React.ReactElement => {
-    const className1: string = 'cursor-pointer text-center border rounded p-3 transition-colors hover:bg-indigo-500 hover:text-white active:bg-indigo-700'
-    const className2: string = 'cursor-pointer text-center border rounded p-3 transition-colors active:bg-indigo-700 bg-indigo-700 text-white ring ring-indigo-300'
+    const className1: string = 'cursor-pointer text-center border rounded p-2 transition-colors hover:bg-indigo-500 hover:text-white active:bg-indigo-700'
+    const className2: string = 'cursor-pointer text-center border rounded p-2 transition-colors active:bg-indigo-700 bg-indigo-700 text-white ring ring-indigo-300'
+    let gender: string = ''
 
     return (
-        <div className='grid grid-rows-3 gap-y-10'>
+        <div className='grid auto-rows-max gap-y-1'>
             { Object.entries(category).filter(arr=>typeof arr[1] === "object").map(arr => arr.map(items => {
-                //console.log(items)
+                if(typeof items === 'string'){
+                    if(items === 'menShoeSize'){
+                        gender = 'Men'
+                    }
+                    if(items === 'womenShoeSize'){
+                        gender = 'Women'
+                    }
+                    if (items === 'kidsShoeSize') {
+                        gender = 'Kids'
+                    }
+                }
                 return(
                     typeof items === 'string' ?
-                    <label key={items} className='text-sm md:text-md font-varela'>{items.toUpperCase()}</label> :
-                    <section className="text-sm text-indigo-800 grid grid-cols-4 max-w-xs gap-2">
+                    <label key={items} className='text-sm md:text-md font-montserrat tracking-wide font-bold'>{gender}</label> :
+
+                    typeof items === 'object' &&
+                    <section key={items.id} className="mb-10 text-xs font-montserrat text-indigo-800 grid grid-cols-4 max-w-xs gap-2">
                     { Object.entries(items).filter(item => typeof item[1] === 'boolean').map(size=> {
+                        let shoeID = gender.concat('-'+size[0]).toLowerCase();
+                        //console.log(shoeID)
                         return(
                             <>
-                                <button>{size[0].toUpperCase().replace('_',' ')}</button>                            
+                                { size[1] ?
+                                <button key={shoeID} name={shoeID} type='button' className={className1} >{size[0].toUpperCase().replace('_',' ')}</button>: 
+                                <button key={shoeID} name={shoeID} type='button' disabled className='text-center border rounded py-2 bg-slate-100 text-slate-400'>{size[0].toUpperCase().replace('_',' ')}</button>
+                                }
+                                {shoeID = ''}
                             </>
                         )
                     })}
                     </section>    
-                )
-
-            }))
-            
-            }
+            )
+            }))}
         </div>
     )
 }
 
 export default SizeGrid
 
+// if(typeof items === 'string'){
+//     if(items === 'menShoeSize'){
+//         gender = 'Men'
+//     }
+//     if(items === 'womenShoeSize'){
+//         gender = 'Women'
+//     }
+//     if (items === 'kidsShoeSize') {
+//         gender = 'Kids'
+//     }
+// }
 
+// return(
+//     typeof items === 'string' ?
+//     <label key={items} className='text-sm md:text-md font-montserrat tracking-wide font-bold'>{gender}</label> :
+
+//     typeof items === 'object' &&
+//     <section key={items.id} className="mb-10 text-xs font-montserrat text-indigo-800 grid grid-cols-4 max-w-xs gap-2">
+//     { Object.entries(items).filter(item => typeof item[1] === 'boolean').map(size=> {
+//         let shoeID = gender.concat('-'+size[0]).toLowerCase();
+//         //console.log(shoeID)
+//         return(
+//             <>
+//                 { size[1] &&
+//                 <button key={shoeID} id={shoeID} type='button' className={className1} >{size[0].toUpperCase().replace('_',' ')}</button>
+//                 }
+//                 {shoeID = ''}
+//             </>
+//         )
+//     })}
+//     </section>    
+// )
+
+
+
+//:
+//<button key={shoeID} id={shoeID} type='button' disabled className='text-center border rounded py-2 bg-slate-100 text-slate-400'>{size[0].toUpperCase().replace('_',' ')}</button>
 // {
 //     //console.log(items)
 //     typeof items[0] === 'string' ?
--- src/router-components/ShoeSolo.tsx
+++ src/router-components/ShoeSolo.tsx
@@ -7,20 +7,20 @@ import SizeGrid from "../components/SizeGrid";
 const ShoeSolo = ({mainData} : {mainData: Shoe []}) : React.ReactElement => {
     const {name_id} = useParams();
     const [errMsg, setErrMsg] = useState<string>('');
-    const [sizeSwitch, setSizeSwitch] = useState<string>('');
+    const [sizeSwitch, setSizeSwitch] = useState<string | null>(null);
 
     return(
         <>
         { mainData.filter(shoe => shoe.name_id === name_id)
             .map(shoe => {
-                console.log(shoe.category.menShoeSize)
+                //console.log(shoe.category.menShoeSize)
                 return (
                     <section key={name_id} className="p-10 w-full flex flex-col items-center md:flex-row md:justify-center">
                         <div>
                             <img alt={shoe.name} src={shoe.img_URL} className='max-h-[40rem]'></img>
                         </div>
-                        <form className='flex flex-col justify-start items-center gap-y-5 md:gap-y-3 lg:gap-y-2'>
-                                <h1 className='text-lg font-sans font-bold text-center'>{shoe.name}</h1>
+                        <div className='flex flex-col justify-start items-center gap-y-5 md:gap-y-3 lg:gap-y-2'>
+                                <h1 className='text-lg font-lato tracking-wide font-bold text-center'>{shoe.name}</h1>
                                 {errMsg === 'no-size-selected' ?
                                     <h3 className={'text-md font-myFont text-red-500'}>No size specified.</h3> :
                                  errMsg === 'duplicate-shoe' ?
@@ -28,7 +28,7 @@ const ShoeSolo = ({mainData} : {mainData: Shoe []}) : React.ReactElement => {
                                     <h3 className='text-md font-myFont text-black opacity-0'>Shoe added.</h3>
                                 }
                                 <SizeGrid category={shoe.category} sizeSwitch={sizeSwitch} setSizeSwitch={setSizeSwitch} />
-                        </form>
+                        </div>
                     </section>
 
                 )
