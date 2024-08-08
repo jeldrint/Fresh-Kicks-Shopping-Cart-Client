7d99f56bea5de0f0c51384f1822f22b8a029c593
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-08-09 07:30:23 +0800

    Update ImageCarousel.tsx

--- src/router-components/ImageCarousel.tsx
+++ src/router-components/ImageCarousel.tsx
@@ -5,10 +5,11 @@ import { Shoe } from "../types/shoetype"
 
 const ImgCarousel = ({mainData}: {mainData: Shoe []}) : React.ReactElement => {
     const [carouselShoes,setCarouselShoes] = useState<Shoe []>([])
+    const tempArr: Shoe [] = mainData;
 
     useEffect(()=> {
         //console.log(mainData.sort((a,b)=>b.discount - a.discount).slice(0,5))
-        setCarouselShoes(mainData.sort((a,b)=>b.discount - a.discount).slice(0,5))
+        setCarouselShoes(tempArr.sort((a,b)=>b.discount - a.discount).slice(0,5))
     },[])
 
     return(
